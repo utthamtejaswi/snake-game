77810b6ece7a523ffaf700598363f54be5d4b73f
Uttham T H <utthamtejaswi@gmail.com>
2021-06-22 17:37:35 +0530

    adding of the snake.
    i have added the snake where in the intial you will have 5 points.(done)
    every time you eat the food teh snake grows by a dot.(will be done soon)

--- snake_game.py
+++ snake_game.py
@@ -8,15 +8,22 @@ screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("snake_game")
 
 
-def text(breadth,length):
+def text(breadth,length,snake_length,snake_breadth,points):
         text=font.render(".",10, (255, 255, 255))
         text_box = ((breadth,length))
         screen.blit(text,text_box)
+
+        text_snake=font.render(points*".",10, (255, 255, 255))
+        text_box_snake = ((snake_breadth,snake_length))
+        screen.blit(text_snake,text_box_snake)
 
 
 done = False
 length=random.randint(0,499)
 breadth=random.randint(0,499)
+snake_length=250
+snake_breadth=250
+points=5
 
 
 while not done:
@@ -25,5 +32,5 @@ while not done:
                         quit()
                         exit()
                                 
-        text(length,breadth)
+        text(length,breadth,snake_length,snake_breadth,points)
         pygame.display.flip()
