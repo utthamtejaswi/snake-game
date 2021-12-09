7768083c2816c01849b907c5712650b424513d12
Uttham T H <utthamtejaswi@gmail.com>
2021-12-09 07:53:25 +0530

    A few new features!!
    This game is now about complete just a few more features to be added.

--- snake_game.py
+++ snake_game.py
@@ -4,14 +4,12 @@ import pygame.font
 
 pygame.init()
 font = pygame.font.Font(None, 36)
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("snake_game")
 
 
-def text(breadth,length,snake_length,snake_breadth,points):
-        text=font.render(".",10, (255, 255, 255))
-        text_box = ((breadth,length))
-        screen.blit(text,text_box)
+def text(food_x,food_y,snake_length,snake_breadth,points):
+        pygame.draw.rect(screen, (0, 255, 0), (food_x,food_y,100,100))
 
         text_snake=font.render(points*".",10, (255, 255, 255))
         text_box_snake = ((snake_breadth,snake_length))
@@ -21,15 +19,21 @@ def text(breadth,length,snake_length,snake_breadth,points):
         text_box_points = ((350,50))
         screen.blit(text_points,text_box_points)
 
+def SquareCollision(rectangleX,rectangleY,pointX,pointY,r1width,r1height):
+        if(rectangleY<pointY) and (pointX>rectangleX) and (pointX<rectangleX+r1width) and (pointY<rectangleY+r1height):
+                return True
+        return False
+
 
 done = False
-length=random.randint(0,450)
-breadth=random.randint(0,450)
+food_y=random.randint(0,450)
+food_x=random.randint(0,450)
 snake_length=250
 snake_breadth=250
 count_length=0
 count_breadth=0
 points=5
+speed=0.08
 
 
 while not done:
@@ -41,14 +45,14 @@ while not done:
                         if event.key==pygame.K_r:
                                 screen.fill((0,0,0))
                                 points=5
-                                length=random.randint(0,450)
-                                breadth=random.randint(0,450)
+                                food_y=random.randint(0,450)
+                                food_x=random.randint(0,450)
                         if event.type == pygame.KEYDOWN:
                                 if event.key==pygame.K_r:
                                         screen.fill((0,0,0))
                                         points=5
-                                        length=random.randint(0,450)
-                                        breadth=random.randint(0,450)
+                                        food_y=random.randint(0,450)
+                                        food_x=random.randint(0,450)
                                 if event.key==pygame.K_UP:
                                         count_length=0
                                         count_breadth=2
@@ -61,17 +65,23 @@ while not done:
                                 if event.key==pygame.K_RIGHT:
                                         count_length=2
                                         count_breadth=1
+
+        if(SquareCollision(snake_length,snake_breadth,food_x,food_y,200,200)):
+                food_y=random.randint(0,450)
+                food_x=random.randint(0,450)
+                points+=1
+                
         if(count_length==0): 
-                snake_length-=0.1
+                snake_length-=speed
                 screen.fill((0,0,0))
         elif(count_length==1):
-                snake_length+=0.1
+                snake_length+=speed
                 screen.fill((0,0,0))
         elif(count_breadth==0):
-                snake_breadth-=0.1
+                snake_breadth-=speed
                 screen.fill((0,0,0))
         elif(count_breadth==1):
-                snake_breadth+=0.1
-                screen.fill((0,0,0))
-        text(length,breadth,snake_length,snake_breadth,points)
+                snake_breadth+=speed
+                screen.fill((0,0,0)) 
+        text(food_y,food_x,snake_length,snake_breadth,points)
         pygame.display.flip()
